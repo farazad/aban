27b37747135c72b83ea81a0048e3d68868e7b20e
farzad <farzaddarvish@yahoo.com>
2024-10-26 00:47:09 +0330

    migrate and some bug fixed

--- accounting/management/commands/init_tokens.py
+++ accounting/management/commands/init_tokens.py
@@ -1,10 +1,7 @@
-from decimal import Decimal
 from django.core.management.base import BaseCommand
 from accounting.models import (
     Asset,
 )
-from user.models import SystemUser
-
 
 class Command(BaseCommand):
     help = "this command creates needed objects for first time running project"
@@ -17,15 +14,16 @@ class Command(BaseCommand):
             name="tether",
             symbol="USDT",
             buy_price = 60000,
-            sell_price = 55000
+            sell_price = 55000,
+            order_volume = 1000
 
         )
         aban = Asset.objects.create(
             name="aban coin",
             symbol="aban",
             buy_price = 4,
-            sell_price = 3
-
+            sell_price = 3,
+            order_volume = 1000
         )
 
         self.stdout.write(self.style.SUCCESS("Successfully created basic data"))
