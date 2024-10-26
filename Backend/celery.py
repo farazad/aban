01d7cb2740fd8db77c000a046f1fa4e1780c5421
farzad <a.darvish@asax.local.ir>
2024-10-26 11:52:36 +0330

    add docker-compose for run project

--- Backend/celery.py
+++ Backend/celery.py
@@ -5,7 +5,7 @@ app = Celery('your_project_name')
 
 app.conf.beat_schedule = {
     'process_pending_transactions': {
-        'task': 'your_app.tasks.process_pending_transactions',
+        'task': 'accounting.tasks.process_pending_transactions',
         'schedule': crontab(minute='*/5'),  # Runs every 5 minutes
     },
 }
